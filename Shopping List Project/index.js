75964fe29a6e2adf7f781f598a3d52eb27dee9e4
nemi <nemimj123@gmail.com>
2023-09-14 15:47:26 +0530

    updated index.js added list-items to shopping list

---
+++ Shopping List Project/index.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form");
+const itemInput = document.querySelector("#addItem");
+const itemList = document.querySelector(".items");
+
+function addItem(e) {
+  e.preventDefault();
+  let newItem = itemInput.value;
+  if (newItem === "") {
+    alert("Please add an item");
+    return;
+  }
+
+  // creating a li element
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton();
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+  itemInput.value = "";
+}
+
+function createButton() {
+  const button = document.createElement("button");
+  const icon = createIcon("fa-solid fa-circle-xmark fa-2x");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+form.addEventListener("submit", addItem);
