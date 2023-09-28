b1a0284f061d4eded71d3f4f58acb97cbb147bbb
nemi <nemimj123@gmail.com>
2023-09-28 12:02:49 +0530

    added fetchAPIData function

--- 10. Flix Movie Project/js/script.js
+++ 10. Flix Movie Project/js/script.js
@@ -5,6 +5,19 @@ const global = {
   currentPage: filename,
 };
 
+// Fetching Data from moviesapi
+
+async function fetchAPIData(endpoint) {
+  const API_KEY = "4c3b7a975301fb951443d3ae9944e6ff";
+  const API_URL = "https://api.themoviedb.db.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+  return data;
+}
+
 function hightlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
   links.forEach((link) => {
