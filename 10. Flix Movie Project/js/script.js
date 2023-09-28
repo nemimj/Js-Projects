7fcc3748b531db7af07f3cfeecc099b6323dfe09
nemi <nemimj123@gmail.com>
2023-09-28 12:52:02 +0530

    created card creation in displaypopularmovies

--- 10. Flix Movie Project/js/script.js
+++ 10. Flix Movie Project/js/script.js
@@ -5,14 +5,43 @@ const global = {
   currentPage: filename,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+  results.forEach((movie) => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="movie-details.html?id=1">
+     ${
+       movie.poster_path
+         ? ` <img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        alt="Movie Title"
+        class="card-img-top"
+      />`
+         : `
+      <img
+      src="/10. Flix Movie Project/images/no-image.jpg"
+      alt="Movie Title"
+      class="card-img-top"
+    />`
+     }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>`;
+
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
 // Fetching Data from moviesapi
-
 async function fetchAPIData(endpoint) {
-  const API_KEY = "4c3b7a975301fb951443d3ae9944e6ff";
-  const API_URL = "https://api.themoviedb.db.org/3/";
-
   const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/${endpoint}?api_key=4c3b7a975301fb951443d3ae9944e6ff`
   );
   const data = await response.json();
   return data;
@@ -32,6 +61,7 @@ function init() {
   switch (global.currentPage) {
     case "index.html":
       console.log("Home");
+      displayPopularMovies();
       break;
     case "shows.html":
       console.log("Shows");
