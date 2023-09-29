88d5090587bfc28d844c602d0c5109858a63fdba
nemi <nemimj123@gmail.com>
2023-09-29 22:47:27 +0530

    updated search function

--- 10. Flix Movie Project/js/script.js
+++ 10. Flix Movie Project/js/script.js
@@ -288,11 +288,56 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if (global.search.term !== "" && global.search.term !== null) {
-    const { results } = await searchAPIData();
-    console.log(results);
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert("No Results Found");
+      return;
+    }
+
+    displaySearchResults(results);
+    document.querySelector("#search-term").value = "";
   } else {
     showAlert("Please enter a search term");
   }
+}
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="${global.search.type}-details.html?id=${result.id}">
+     ${
+       result.poster_path
+         ? ` <img
+        src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+        alt="${global.search.type === "movie" ? result.title : result.name}"
+        class="card-img-top"
+      />`
+         : `
+      <img
+      src="/10. Flix Movie Project/images/no-image.jpg"
+      alt="${global.search.type === "movie" ? result.title : result.name}"
+      class="card-img-top"
+    />`
+     }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${
+        global.search.type === "movie" ? result.title : result.name
+      }</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${
+          global.search.type === "movie"
+            ? result.release_date
+            : result.first_air_date
+        }</small>
+      </p>
+    </div>`;
+
+    document.querySelector("#search-results").appendChild(div);
+  });
 }
 
 // search for a term using api
@@ -310,7 +355,7 @@ async function searchAPIData() {
 
 // show Alert
 
-function showAlert(message, className) {
+function showAlert(message, className = "error") {
   const alertEl = document.createElement("div");
   alertEl.classList.add("alert", className);
   alertEl.appendChild(document.createTextNode(message));
